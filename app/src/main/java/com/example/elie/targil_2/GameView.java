ee0aef5ed349f9271a7f676c2faf6f02bf7de0c0
elie <zerbib16@gmail.com>
2018-12-04 10:08:06 +0200

    commit test

---
+++ app/src/main/java/com/example/elie/targil_2/GameView.java
@@ -0,0 +1,21 @@
+package com.example.elie.targil_2;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GameView extends View {
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+    }
+    public GameView(Context context) {
+        super(context);
+    }
+}
